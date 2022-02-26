7836d2e5172e558f5d6fcbed2780ce52e4d39cb6
henricaodopao1 <dragonsxbr@gmail.com>
2022-02-26 14:26:12 +0000

    Revert "[Q_SDM636_Ara_dev]:update kernel"
    This reverts commit aa8a328a97b9c9dc3bf5f09358d2f103173df966.

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -159,7 +159,7 @@ int scm_set_dload_mode(int arg1, int arg2)
 				&desc);
 }
 
-void set_dload_mode(int on)
+static void set_dload_mode(int on)
 {
 	int ret;
 
@@ -169,7 +169,6 @@ void set_dload_mode(int on)
 		       dload_mode_addr + sizeof(unsigned int));
 		mb();
 	}
-	pr_err("[kk]set dload mode\n");
 
 	ret = scm_set_dload_mode(on ? dload_type : 0, 0);
 	if (ret)
@@ -246,9 +245,8 @@ EXPORT_SYMBOL(set_QPSTInfo_dloadmode);
 // ASUS BSP --- set download_mode when writing QPSTInfo
 
 #else
-void set_dload_mode(int on)
+static void set_dload_mode(int on)
 {
-    printfk("set_dload_mode return;");
 	return;
 }
 
